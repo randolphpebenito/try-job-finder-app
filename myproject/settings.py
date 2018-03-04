126efd44d3741f55809f22e2dc749fcabf8725d2
Randolph Pe Benito <rpebenito@xyber.ph>
2018-03-05 02:18:24 +0800

    Update settings and gitignore.

--- myproject/settings.py
+++ myproject/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
